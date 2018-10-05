5bd7827380f857f4112ff365c3982fc06a46cd61
dcollins4096 #Jane Doe <jdoe@example.com>
2018-10-05 09:09:12 -0400

    reorg

--- fsu_research/ResearchPageMaker.py
+++ fsu_research/ResearchPageMaker.py
@@ -1,60 +1,16 @@
 
 import jinja2
-
+import physics_data
 class group():
     def __init__(self, name="MOON",image="NO.png",web="google.com"):
         self.name=name
         self.image=image
         self.web=web
 
-ResearchGroupInfo={}
-ResearchGroupInfo['atomic']={'web':"https://physics.fsu.edu/research/atomic-physics",
-                 'image':"Research_Atomic.png",
-                 'name':"Atomic Physics"}
-ResearchGroupInfo['CME']={'web':"https://physics.fsu.edu/research/condensed-matter-experimental-physics",
-'image':"Research_CM_X.png",
-'name': "Condensed Matter Experiment"}
-ResearchGroupInfo['NT']={'web':"http://wwwphysics.fsu.acsitefactory.com/nuclear-theory",
-          'image':"Research_NUC_T.jpg" ,
-          'name':"Nuclear Theory"}
-ResearchGroupInfo['Hadronic']={'web':"http://hadron.physics.fsu.edu/",
-          'image':"Research_NUC_Had.png" ,
-          'name':"Nuclear: Hadronic"}
-ResearchGroupInfo['CMT']={'web':"https://physics.fsu.edu/research/condensed-matter-theoretical-physics",
-              'image':"Research_CM_T.jpg",
-              'name':"Condensed Matter Theory"}
-ResearchGroupInfo['HET']={'web':"http://www.hep.fsu.edu/",
-              'image':"Research_HE_T.png",
-              'name':"High Energy Physics: Theory"}
-ResearchGroupInfo['HEE']={'web':"http://www.hep.fsu.edu/",
-          'image':"Research_HE_X.jpg",
-          'name':"High Energy Physics: Experiment"}
-ResearchGroupInfo['NHM']={'web':"https://magnet.fsu.edu",
-          'image':"Research_MAG.jpg" ,
-          'name':"National High Magnetic Field Lab"}
-ResearchGroupInfo['Fox']={'web':"http://fsunuc.physics.fsu.edu/",
-          'image':"Research_NUC_X.jpg" ,
-          'name':"Nuclear: John D. Fox Accelerator Lab"}
-ResearchGroupInfo['astro']={'web':"Astrophysics",'image':"Research_Astro.png", 'name':"http://astrophysics.physics.fsu.edu/"))
+group_info = physics_data.group_info
 g=[]
-g.append(group(***ResearchGroupInfo['astro']))
-g.append(
-    group(**ResearchGroupInfo['atomic']))
-g.append(
-    group(**ResearchGroupInfo['CME']))
-g.append(
-    group(**ResearchGroupInfo['CMT']))
-g.append(
-    group(**ResearchGroupInfo['HET']))
-g.append(
-    group(**ResearchGroupInfo['HEE']))
-g.append( group(**ResearchGroupInfo['NHM']))
-g.append(
-    group(**ResearchGroupInfo['Fox']))
-g.append(
-          group(**ResearchGroupInfo['NT']))
-g.append(
-    group(**ResearchGroupInfo['Hadronic']))
+for name in group_info:
+    g.append(group(**group_info[name]))
 
 """Sometimes the template used
 <div class="clearfix visible-xs visible-sm visible-lg"></div>
